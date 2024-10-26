27228c323b820462b3b9b2a8448d38565b5d1d75
SebD6994 <sebastien.detournay@gmail.com>
2024-10-27 01:16:12 +0200

    crash xammp

--- app/Config/config.php
+++ app/Config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'drdupont');
+define('DB_NAME', 'cabinet');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
--- assets/js/admin.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Confirmation de suppression
-    window.confirmDelete = function(message) {
-        return confirm(message); // Affiche une boîte de confirmation
-    };
-});
